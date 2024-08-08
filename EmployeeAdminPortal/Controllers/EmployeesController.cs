5141a77ae6d5110782a93ecdac5ab70ffd9f5ae4
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-08-08 19:23:44 +0300

    v1.0.1

--- EmployeeAdminPortal/Controllers/EmployeesController.cs
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -1,4 +1,6 @@
 ﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models.Dtos.Employee;
+using EmployeeAdminPortal.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,9 +17,54 @@ namespace EmployeeAdminPortal.Controllers
         }
 
         [HttpGet]
+       
         public IActionResult GetAllEmployees()
         { 
             return Ok(_datacontext.Employees.ToList());
         }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id) {
+           var employee= _datacontext.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto) {
+
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary,
+            };
+
+            _datacontext.Employees.Add(employeeEntity);
+            _datacontext.SaveChanges();
+            return Ok(employeeEntity);
+        }
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id, UpdateEmployeeDto employee )
+        {
+            var employeeDb = _datacontext.Employees.Find(id);
+            if (employeeDb == null) { 
+                return NotFound();
+            }
+            employeeDb.Name=employee.Name;
+            employeeDb.Email=employee.Email;
+            employeeDb.Phone=employee.Phone;
+            employeeDb.Salary=employee.Salary;
+            _datacontext.SaveChanges();
+            return Ok(employeeDb);
+        }
+
     }
 }
